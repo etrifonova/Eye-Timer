14fa5b14638faa64e59aa7949c3348172d12f3fa
Elena Trifonova <101653327+etrifonova@users.noreply.github.com>
2023-12-08 23:32:13 +0300

    feat: finish timer

--- eye-timer.js
+++ eye-timer.js
@@ -1,24 +1,29 @@
+const alarmSound = new Audio("./alarm.mp3"); // Replace with path to your sound file
+const display = document.querySelector("#countdown");
+// const fiveMinutes = 60 * 0.1;
+let timer = 1201;
+display.textContent = "20:00";
+
 function startTimer() {
-    var interval = 20 * 60; // interval in seconds between alarms
-    var timeLeft = interval;
-    var alarmSound = new Audio('./alarm.mp3'); // Replace with path to your sound file
+
+  if (timer > 0) {
   
-    function countdown() {
-      var minutes = Math.floor(timeLeft / 60);
-      var seconds = timeLeft % 60;
-      var countdownDisplay = document.getElementById("countdown");
-      countdownDisplay.innerHTML = "Time left: " + minutes + "m " + seconds + "s";
-      if (timeLeft <= 0) {
-        clearInterval(countdownInterval);
-        alarmSound.play(); // Play the alarm sound
-        timeLeft = interval;
-        countdownDisplay.innerHTML = "";
-        countdown();
-      }
-      timeLeft--;
-    }
+    timer--;
+    let minutes = parseInt(timer / 60, 10);
+    let seconds = parseInt(timer % 60, 10);
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    let timeLeft = minutes + ":" + seconds;
   
-    countdown();
-    var countdownInterval = setInterval(countdown, 1000);
+    display.textContent = timeLeft;
+    console.log(timeLeft)
+    setTimeout(startTimer, 1000);
+
   }
-  
+    else if (timer == 0) {
+      alarmSound.play();
+      console.log("bro");
+      timer = 1201;
+      setTimeout(startTimer, 20000);
+    }
+}
